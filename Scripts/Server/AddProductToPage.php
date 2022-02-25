ea439e2e4ed69fc0fcad6d202a3d25637fc53bef
willcorkill13 <corkillw@gmail.com>
2022-02-25 13:14:10 +0000

    Write a script to process AJAX requests and display the cart on the page

---
+++ Scripts/Server/AddProductToPage.php
@@ -0,0 +1,39 @@
+<?php
+    function getDatabaseConnection()
+    {
+        $dbServerName = "localhost";
+        $username = "root";
+        $password = "password";
+        $connection = new mysqli($dbServerName, $username, $password, "web_technologies_ass2");
+        while ($connection->connect_error)
+        {
+            $connection = new mysqli($dbServerName, $username, $password, "web_technologies_ass2");
+        }
+        return $connection;
+    }
+
+    $prices = array();
+
+    $dbConnection = getDatabaseConnection();
+    $prepareProductStatement = $dbConnection->prepare(
+        "SELECT products.product_id, products.colour, products.product_image, products.product_type, productTypes.price
+        FROM products 
+
+        INNER JOIN productTypes
+        ON productTypes.productType = products.product_type
+        AND products.product_type = ?
+        AND products.colour = ?;
+        "
+    );
+    $prepareProductStatement->bind_param('ss', $_POST["product_type"], $_POST["product_colour"]);
+    $prepareProductStatement->execute();
+    $result = $prepareProductStatement->get_result();
+    $data_row = $result->fetch_assoc();
+    echo '<div class="row" id="' . $_POST["product_type"] . $_POST["product_colour"] . '">';
+    echo '<p>' . $data_row["product_id"] . '</p>';
+    echo '<img src=' . $data_row["product_image"] . '>';
+    echo '<p>' . $data_row["colour"] . ' ' . $data_row["product_type"] . '</p>';
+    echo '<p>£' . $data_row["price"] . '</p>';
+    echo '<button onclick="removeFromCart(\'' . $_POST["product_type"] . $_POST["product_colour"] . '\', \'' . $_POST["localStorageKey"] . '\')">Remove</button>';
+    echo '</div>';
+?>
