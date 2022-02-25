3158d379b68b330a9a79c212e8df88edfc09ee66
willcorkill13 <corkillw@gmail.com>
2022-02-25 18:50:44 +0000

    Update the add to page PHP script to take the product ID instead of colour and product type

--- Scripts/Server/AddProductToPage.php
+++ Scripts/Server/AddProductToPage.php
@@ -12,28 +12,23 @@
         return $connection;
     }
 
-    $prices = array();
-
     $dbConnection = getDatabaseConnection();
     $prepareProductStatement = $dbConnection->prepare(
         "SELECT products.product_id, products.colour, products.product_image, products.product_type, productTypes.price
         FROM products 
-
         INNER JOIN productTypes
         ON productTypes.productType = products.product_type
-        AND products.product_type = ?
-        AND products.colour = ?;
-        "
+        AND products.product_id = ?;"
     );
-    $prepareProductStatement->bind_param('ss', $_POST["product_type"], $_POST["product_colour"]);
+    $prepareProductStatement->bind_param('s', $_POST["product_id"]);
     $prepareProductStatement->execute();
     $result = $prepareProductStatement->get_result();
     $data_row = $result->fetch_assoc();
-    echo '<div class="row" id="' . $_POST["product_type"] . $_POST["product_colour"] . '">';
+    echo '<div class="row" id="' . $data_row["product_type"] . $data_row["colour"] . '">';
     echo '<p>' . $data_row["product_id"] . '</p>';
-    echo '<img src=' . $data_row["product_image"] . '>';
+    echo '<img src=\'' . $data_row["product_image"] . '\'>';
     echo '<p>' . $data_row["colour"] . ' ' . $data_row["product_type"] . '</p>';
     echo '<p>£' . $data_row["price"] . '</p>';
-    echo '<button onclick="removeFromCart(\'' . $_POST["product_type"] . $_POST["product_colour"] . '\', \'' . $_POST["localStorageKey"] . '\')">Remove</button>';
+    echo '<button onclick="removeFromCart(\'' . $data_row["product_type"] . $data_row["colour"] . '\', \'' . $_POST["localStorageKey"] . '\')">Remove</button>';
     echo '</div>';
 ?>
