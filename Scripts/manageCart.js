4f692cd7fcd31ed084a6b00bce598530c98771a1
willcorkill13 <corkillw@gmail.com>
2022-02-25 22:15:46 +0000

    Create client-sided purchase script

--- Scripts/manageCart.js
+++ Scripts/manageCart.js
@@ -27,36 +27,78 @@ function removeFromCart(containerId, itemIdentifier)
 
 function SendCartItemToServer(container, product_id, localStorageKey) {
     var XmlHttp = new XMLHttpRequest();
-    XmlHttp.onreadystatechange = function() {
-    }
     XmlHttp.open("POST", "../Scripts/Server/AddProductToPage.php", false)
     XmlHttp.onload = function() {
         container.innerHTML += this.responseText;
     }
     XmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); // Allow to send post request such as in a form
-    XmlHttp.send(`product_id=${product_id}&&localStorageKey=${localStorageKey}`);
+    XmlHttp.send(`product_id=${product_id}&localStorageKey=${localStorageKey}`);
+}
+
+function performCheckout() {
+    var countItems = 0;
+    var itemPostString = "orderItems=";
+    const storageKeys = Object.keys(localStorage).sort(); // Sort the keys into alphabetical order
+    for (const key in storageKeys) 
+    {
+        const itemKey = storageKeys[key];
+        if (itemKey.search("UCLan") != -1)
+        {
+            countItems++;
+            itemPostString += String(localStorage.getItem(itemKey)) + "+";
+        }
+    }
+    // Submit information to the web server for database changes
+    if (countItems > 0)
+    {
+        itemPostString = itemPostString.slice(0, -1);
+        var XmlHttp = new XMLHttpRequest();
+        XmlHttp.open("POST", "../Scripts/Server/Checkout.php", true)
+        XmlHttp.onload = function() {
+            if (this.responseText == "success")
+            {
+                alert("Your purchase has been successful, thank you for shopping with the UCLan Student's Union.");
+                localStorage.clear();
+                window.location.reload();
+            }
+            else
+            {
+                alert("Your purchase was unsuccessful, please try again later");
+            }
+        }
+        XmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); // Allow to send post request such as in a form
+        console.log(itemPostString)
+        XmlHttp.send(itemPostString);
+    }
+    else
+    {
+        alert("There are no items in your cart! Please visit our products page to our products page to add items to your cart.");
+    }
 }
 
 window.addEventListener("load", function() {
     tableContainer = document.getElementById("table");
-    const storageKeys = Object.keys(localStorage).sort(); // Sort the keys into alphabetical order
-    if (storageKeys.length < 1) // If there are no items in the cart
+    if (tableContainer)
     {
-        const noItemsNotifier = document.createElement("p");
-        noItemsNotifier.setAttribute("class", "noItems");
-        noItemsNotifier.innerHTML = "There are no items in your cart!";
-        tableContainer.append(noItemsNotifier);
-    }
-    else
-    { // If there are one or more items in the cart
-        for (const key in storageKeys) 
+        const storageKeys = Object.keys(localStorage).sort(); // Sort the keys into alphabetical order
+        if (storageKeys.length < 1) // If there are no items in the cart
         {
-            const itemKey = storageKeys[key];
-            console.log(itemKey.search("UCLan"));
-            if (itemKey.search("UCLan") != -1)
+            const noItemsNotifier = document.createElement("p");
+            noItemsNotifier.setAttribute("class", "noItems");
+            noItemsNotifier.innerHTML = "There are no items in your cart!";
+            tableContainer.append(noItemsNotifier);
+        }
+        else
+        { // If there are one or more items in the cart
+            for (const key in storageKeys) 
             {
-                SendCartItemToServer(tableContainer, localStorage.getItem(itemKey), itemKey);
+                const itemKey = storageKeys[key];
+                if (itemKey.search("UCLan") != -1)
+                {
+                    SendCartItemToServer(tableContainer, localStorage.getItem(itemKey), itemKey);
+                }
             }
         }
     }
+
 });
