f256b1de71338b75a728a6c23e7da7b57cf4b125
willcorkill13 <corkillw@gmail.com>
2022-02-25 13:14:29 +0000

    Change the client script to send information to the server instead of generating it on the client

--- Scripts/manageCart.js
+++ Scripts/manageCart.js
@@ -15,7 +15,7 @@ function parseValue(localStorageValue)
 function addToCart(productType, productColour) {
     // Function to be written for adding to the cart
     const numberOfKeys = Object.keys(localStorage).length;
-    localStorage.setItem(`item${numberOfKeys+1}`, `${productType},${productColour}`); // The item in the local storage is assigned to be used on cart.html and loadCart.js
+    localStorage.setItem(`UCLan_Item${numberOfKeys+1}`, `${productType},${productColour}`); // The item in the local storage is assigned to be used on cart.html and loadCart.js
     alert(`${productColour} ${productType} has been added to your cart.`); // Show an alert informing the user of the addition to their cart.
 }
 function removeFromCart(containerId, itemIdentifier) 
@@ -25,47 +25,19 @@ function removeFromCart(containerId, itemIdentifier)
     container.remove(); // Remove the item from cart.html
 }
 
-function createRow(itemKey, localStorageValue) 
-{
-    // Gather all information needed to create the row in the list.
-    const productInfo = parseValue(localStorageValue); // Split the value into two separate values in an array
-    const productType = productInfo[0]; // Index the array of values to ascertain the product type and product colour
-    const productColour = productInfo[1];
-    // Create the container for the row
-    const rowContainer = document.createElement("div");
-    rowContainer.className = "row";
-    rowContainer.id = `row${rowId}`;
-    // Create item identifier to be presented to the user
-    const itemIdentifier = document.createElement("p");
-    itemIdentifier.textContent = itemKey.slice(-1); // Index the final character in the itemKey
-    rowContainer.appendChild(itemIdentifier);
-    // Show the user an image
-    const itemImage = document.createElement("img");
-    itemImage.src = `../Images/${productType}/${productColour.replaceAll(" ", "%20")}.jpg`; // (JavaScriptTutorial, n.d.) regarding replaceAll function
-    itemImage.alt = `Photo of a ${productColour} ${productType}`;
-    rowContainer.appendChild(itemImage);
-    // Show the product name
-    const itemName = document.createElement("p");
-    itemName.textContent = `${productColour} ${productType}`;
-    rowContainer.appendChild(itemName);
-    // Show the product price
-    const itemPrice = document.createElement("p");
-    itemPrice.textContent = `${prices[productType]}`;
-    rowContainer.appendChild(itemPrice);
-    // Create a remove button
-    const removeContainer = document.createElement("div");
-    removeContainer.className = "removeContainer";
-    rowContainer.appendChild(removeContainer);
-    const removeItem = document.createElement("button");
-    removeItem.textContent = "Remove";
-    removeItem.setAttribute(`onclick` ,`removeFromCart("row${rowId}", "${itemKey}")`);
-    removeContainer.appendChild(removeItem);
-    // Add the container to the table
-    rowId++; // Increment the value of rowId
-    tableContainer.appendChild(rowContainer);
+function SendCartItemToServer(container, product_type, product_colour, localStorageKey) {
+    var XmlHttp = new XMLHttpRequest();
+    XmlHttp.onreadystatechange = function() {
+    }
+    XmlHttp.open("POST", "../Scripts/Server/AddProductToPage.php", false)
+    XmlHttp.onload = function() {
+        container.innerHTML += this.responseText;
+    }
+    XmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); // Allow to send post request such as in a form
+    XmlHttp.send(`product_type=${product_type}&product_colour=${product_colour}&localStorageKey=${localStorageKey}`);
 }
 
-/*window.addEventListener("load", function() {
+window.addEventListener("load", function() {
     tableContainer = document.getElementById("table");
     const storageKeys = Object.keys(localStorage).sort(); // Sort the keys into alphabetical order
     if (storageKeys.length < 1) // If there are no items in the cart
@@ -80,8 +52,13 @@ function createRow(itemKey, localStorageValue)
         for (const key in storageKeys) 
         {
             const itemKey = storageKeys[key];
-            const itemValue = localStorage.getItem(itemKey);
-            createRow(itemKey, itemValue);
+            console.log(itemKey.search("UCLan"));
+            if (itemKey.search("UCLan") != -1)
+            {
+                const itemValue = localStorage.getItem(itemKey);
+                const productInfo = parseValue(itemValue)
+                SendCartItemToServer(tableContainer, productInfo[0], productInfo[1], itemKey);
+            }
         }
     }
-});*/
+});
