142477ae10aacf86b386898d36c1e00409214d92
willcorkill13 <corkillw@gmail.com>
2022-02-26 18:29:17 +0000

    Amend the get_next_primary_key function due to erroneous input

--- Scripts/Server/Database.php
+++ Scripts/Server/Database.php
@@ -46,10 +46,10 @@ function get_next_primary_key($table_name)
     // Open the database connection
     $database_connection = database_connect();
     // Write the statement
-    $sql_next_primary_key = "SELECT auto_increment FROM INFORMATION_SCHEME.TABLES WHERE table_names = '" . $table_name . "';";
+    $sql_next_primary_key = "SELECT auto_increment FROM INFORMATION_SCHEMA.TABLES WHERE table_name = '" . $table_name . "';";
     $query_next_primary_key = $database_connection->query($sql_next_primary_key);
     // Get the data
-    $next_primary_key = $query_next_primary_key->fetch_row()[0];
-    return $next_primary_key;
+    $next_primary_key = $query_next_primary_key->fetch_row();
+    return $next_primary_key[0];
 }
 ?>
