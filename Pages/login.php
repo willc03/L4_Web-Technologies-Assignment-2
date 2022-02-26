805fd5a87c8d635ecd923474081f34c483560baa
willcorkill13 <corkillw@gmail.com>
2022-02-26 18:29:46 +0000

    Make changes to be compatible with style.css

--- Pages/login.php
+++ Pages/login.php
@@ -9,7 +9,7 @@
 <?php
     session_start(); // Allow the page to make use of the $_SESSION variable
     require '../Scripts/Server/Database.php';
-    if (isset($_POST["submitType"]) and $_POST["submitType"] == "Log in") // Check if the user has pressed log in
+    if (isset($_POST["submit_type"]) and $_POST["submit_type"] == "Log in") // Check if the user has pressed log in
     {
         // Get the information from the form
         $email = $_POST["loginEmail"];
@@ -20,10 +20,9 @@
             array("s", $email)
         );
         // Check if user details exist and if they do, get user details
-        if (!$user_result->num_rows > 0)
+        if ($user_result->num_rows == 0)
         {
             $doesEmailExist = false;
-            exit();
         }
         else
         {
@@ -40,20 +39,20 @@
             }
         }
     }
-    if (isset($_POST["submitType"]) and $_POST["submitType"] == "Sign Up")
+    if (isset($_POST["submit_type"]) and $_POST["submit_type"] == "Sign Up")
     {
         // Get form details
         $email = $_POST["email"];
         // Execute an inject-protected statement
-        $create_user_details = prepare_statement(
+        $get_existing_user_details = prepare_statement(
             "SELECT COUNT(user_id) FROM users WHERE user_email = ?;",
             array("s", $email)
-        )
+        );
         // Check if the email already exists in the system
-        if ($create_user_details->num_rows > 0)
+        $existing_row = $get_existing_user_details->fetch_row();
+        if ($existing_row[0] > 0)
         {
             $emailAlreadyExists = true;
-            exit();
         }
         else
         {
@@ -62,11 +61,11 @@
             $date_time = new DateTime();
             $formatted_time = $date_time->format("Y-m-d H:i:s");
             // Prepare statement
-            $sql_insert_data = prepare_statement(
+            $create_new_details = prepare_statement(
                 "INSERT INTO users (user_id, user_full_name, user_address, user_email, user_pass, user_timestamp) VALUES (?, ?, ?, ?, ?, ?)",
                 array("isssss", $next_user_id, $_POST["fullName"], $_POST["address"], $_POST["email"], password_hash($_POST["password"], PASSWORD_BCRYPT), $formatted_time)
-            )
-            $newAccount = true;
+            );
+            $new_account = true;
         }
     }
 ?>
@@ -75,14 +74,13 @@
 
     <head> <!-- Content in the head of the document invisible to the user -->
         <title>Login - UCLan Student's Union Shop</title> <!-- Sets the name of the tab in the browser -->
-        <link type="text/css" rel="stylesheet" href="../Stylesheets/bootstrap.min.css"> <!-- The Bootstrap version 5 stylesheet -->
-        <link type="text/css" rel="stylesheet" href="../Stylesheets/global.css"> <!-- Style the header & footer of the page -->
-        <link type="text/css" rel="stylesheet" href="../Stylesheets/index.css"> <!-- The index sheet is used as the characteristics are similar to what is needed
         <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- Enable media queries & define charset -->
         <meta charset="utf-8">
-        <script src="../Scripts/navigation.js"></script> <!-- Used to configure the hamburger menu -->
-        <script src="../Scripts/formValidation.js"></script> <!-- Used to locally validate forms -->
-        <script src="../Scripts/ajaxRequests.js"></script> <!-- Ajax is used to communicate between the client and the server -->
+        <link type="text/css" rel="stylesheet" href="../Stylesheets/bootstrap.min.css"> <!-- The Bootstrap version 5 stylesheet -->
+        <link type="text/css" rel="stylesheet" href="../style.css">
+        <script src="../Scripts/Client/Navigation.js"></script> <!-- Used to configure the hamburger menu -->
+        <script src="../Scripts/Client/formValidation.js"></script> <!-- Used to locally validate forms -->
+        <script src="../Scripts/Client/UserAccountRequest.js"></script> <!-- Ajax is used to communicate between the client and the server -->
         <script src="../Scripts/bootstrap.bundle.min.js"></script> <!-- The Bootstrap version 5 latest scripts -->
     </head>
 
@@ -107,17 +105,17 @@
             
             <div id="main"> <!-- Defines the main content of the page, below the header, above the footer. -->
                 <?php
-                    if ((!isset($newAccount)) and (isset($_POST["submitType"]) and in_array($_POST["submitType"], array("Sign up here", "Sign Up")))) // If the user has pressed sign up
+                    if ((!isset($new_account)) and (isset($_POST["submit_type"]) and in_array($_POST["submit_type"], array("Sign up here", "Sign Up")))) // If the user has pressed sign up
                     {
                         echo '<br><h2 class="sub_title">Sign Up</h2><br>';
                         if (isset($emailAlreadyExists))
                         {
-                            echo '<div id="incorrectDetails">';
+                            echo '<div id="incorrect_details">';
                             echo '<h2><strong>Email in use</strong></h2>';
                             echo '<p>The email entered is already registered to an account on our website. Please try again.</p>';
                             echo '</div>';
                         }
-                        echo '<form method="post" id="signup">';
+                        echo '<form method="post" id="sign_up_form">';
                         echo '<label for="fullName">Full name: </label><br>';
                         echo '<input type="text" name="fullName" required><br>';
 
@@ -127,12 +125,12 @@
                         echo '<label for="address">Address: </label><br>';
                         echo '<input type="text" name="address" required>';
 
-                        echo '<div id="passwordContainer">';
+                        echo '<div id="password_container">';
 
                         echo '<br><label for="password">Password: </label>';
                         echo '<input type="password" name="password" id="password" minlength="8" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required>';
 
-                        echo '<ul id="passwordRequirements">';
+                        echo '<ul id="password_requirements">';
                         echo '<p class="requirements">Password requirements:</p><br>';
                         echo '<li class="invalid" id="length">Contain at least 8 characters</li>';
                         echo '<li class="invalid" id="uppercase">Contain at least 1 upper-case character</li>';
@@ -144,23 +142,23 @@
                         echo '<input type="password" name="confirmPassword" id="confirmPassword" required>';
 
                         echo '</div><br>';
-                        echo '<input type="submit" value="Sign Up" name="submitType">';
+                        echo '<input type="submit" value="Sign Up" name="submit_type">';
                         echo '</form>';
                     }
                     else // If the user has loaded the page for the first time or has pressed log in
                     {
-                        echo '<h1 class="title">Log in</h1>';
+                        echo '<h2 class="title">Log in</h2>';
                         echo '<p>Logging into our website will allow you to order items at the checkout.</p>';
-                        if (isset($newAccount))
+                        if (isset($new_account))
                         {
-                            echo '<div id="newAccount">';
+                            echo '<div id="new_account">';
                             echo '<h2><strong>Account Created</strong></h2>';
                             echo '<p>Your account has been successfully created. Please log in using the form below.</p>';
                             echo '</div>';
                         }
-                        else if (isset($_POST["submitType"])) // Add necessary feedback for incorrect details.
+                        else if (isset($_POST["submit_type"])) // Add necessary feedback for incorrect details.
                         {
-                            echo '<div id="incorrectDetails">';
+                            echo '<div id="incorrect_details">';
                             if (isset($doesEmailExist) and !$doesEmailExist) // This variable was created in the first section of PHP
                             {
                                 echo '<h2><strong>Invalid Email</strong></h2>';
@@ -173,16 +171,16 @@
                             }
                             echo '</div>';
                         }
-                        echo '<form method="post" class="loginForm">';
+                        echo '<form method="post" id="log_in_form">';
                         echo '<label for="loginEmail">Email: </label><br>';
                         echo '<input type="email" name="loginEmail" required><br>';
                         echo '<label for="loginPassword">Password: </label><br>';
                         echo '<input type="password" name="loginPassword" required>';
-                        echo '<br><br><input type="submit" name="submitType" value="Log in" id="loginButton"><br>';
+                        echo '<br><br><input type="submit" name="submit_type" value="Log in" id="loginButton"><br>';
                         echo '<label for="signUp">Don\'t have an account?</label>';
                         echo '</form>';
-                        echo '<form method="post" class="loginForm">';
-                        echo '<input type="submit" name="submitType" value="Sign up here" id="signUpButton">';
+                        echo '<form method="post" id="log_in_form">';
+                        echo '<input type="submit" name="submit_type" value="Sign up here" id="signUpButton">';
                         echo '</form>';
                     }
                 ?>
