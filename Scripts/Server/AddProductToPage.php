e6b6120acaa445e00719cb39ed52850ee6976c3a
willcorkill13 <corkillw@gmail.com>
2022-03-02 12:25:30 +0000

    Update to use new database functions

--- Scripts/Server/AddProductToPage.php
+++ Scripts/Server/AddProductToPage.php
@@ -1,29 +1,12 @@
 <?php
-    function getDatabaseConnection()
-    {
-        $dbServerName = "localhost";
-        $username = "root";
-        $password = "password";
-        $connection = new mysqli($dbServerName, $username, $password, "web_technologies_ass2");
-        while ($connection->connect_error)
-        {
-            $connection = new mysqli($dbServerName, $username, $password, "web_technologies_ass2");
-        }
-        return $connection;
-    }
-
-    $dbConnection = getDatabaseConnection();
-    $prepareProductStatement = $dbConnection->prepare(
-        "SELECT products.product_id, products.colour, products.product_image, products.product_type, productTypes.price
-        FROM products 
-        INNER JOIN productTypes
-        ON productTypes.productType = products.product_type
-        AND products.product_id = ?;"
+    // Get useful database function
+    require '../Server/Database.php';
+    // Connect to the database
+    $prepare_product_statement = prepare_statement(
+        "SELECT products.*, productTypes.price FROM products INNER JOIN productTypes ON productTypes.productType = products.product_type AND products.product_id = ?;",
+        array("s", $_POST["product_id"])
     );
-    $prepareProductStatement->bind_param('s', $_POST["product_id"]);
-    $prepareProductStatement->execute();
-    $result = $prepareProductStatement->get_result();
-    $data_row = $result->fetch_assoc();
+    $data_row = $prepare_product_statement->fetch_assoc();
     echo '<div class="table_row" id="' . $data_row["product_type"] . $data_row["colour"] . '">';
     echo '<p>' . $data_row["product_id"] . '</p>';
     echo '<img src=\'' . $data_row["product_image"] . '\'>';
