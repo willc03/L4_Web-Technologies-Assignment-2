f3e361aa6a5337ab0f02752eecf5a901911f7547
willcorkill13 <corkillw@gmail.com>
2022-03-02 12:25:41 +0000

    Update to use new database functions

--- Scripts/Server/Checkout.php
+++ Scripts/Server/Checkout.php
@@ -1,30 +1,15 @@
 <?php
+    // Start the session and get database functions
     session_start();
-    function getDatabaseConnection()
-    {
-        $dbServerName = "localhost";
-        $username = "root";
-        $password = "password";
-        $connection = new mysqli($dbServerName, $username, $password, "web_technologies_ass2");
-        while ($connection->connect_error)
-        {
-            $connection = new mysqli($dbServerName, $username, $password, "web_technologies_ass2");
-        }
-        return $connection;
-    }
-
-    $dbConnection = getDatabaseConnection();
-
-    $prepareCheckoutStatement = $dbConnection->prepare("INSERT INTO orders (order_id, user_id, product_ids) VALUES (?, ?, ?);");
-    $prepareCheckoutStatement->bind_param('iis', $order_id, $_SESSION["id"], ($_POST["orderItems"]));
-
-    //$dateTime = new DateTime();
-    //$order_date = $dateTime->format('Y-m-d H:i:s');
-
-    $sqlGetNextOrderId = "SELECT auto_increment FROM INFORMATION_SCHEMA.TABLES WHERE table_name = 'orders';";
-    $order_id = ($dbConnection->query($sqlGetNextOrderId))->fetch_row()[0]; // Get the next value
-
-    if ($prepareCheckoutStatement->execute())
+    require '../Server/Database.php';
+    // Run a secured statement
+    $next_order_id = get_next_primary_key("orders");
+    $prepare_checkout_statement = prepare_statement(
+        "INSERT INTO orders (order_id, user_id, product_ids) VALUES (?, ?, ?);",
+        array("iis", $next_order_id, $_SESSION["id"], $_POST["orderItems"])
+    );
+    // Check success
+    if ($prepare_checkout_statement != "failed")
     {
         echo "success";
     }
